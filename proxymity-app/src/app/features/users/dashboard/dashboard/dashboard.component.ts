78942eb212a3ecb429472b04560445dbe656ad4d
Tom Salisbury <Tom.Salisbury@oneiota.co.uk>
2024-10-30 12:42:03 +0000

    update dashboard component

--- proxymity-app/src/app/features/users/dashboard/dashboard/dashboard.component.ts
+++ proxymity-app/src/app/features/users/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { UserService } from 'src/core/services/user.service';
+import { IUser } from 'src/shared/models/user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: []
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  users$: Observable<IUser[]>;
 
+  constructor(private userService: UserService, private router: Router) {
+    this.users$ = this.userService.users$;
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe();
+  }
+
+  trackByUserId(index: number, user: IUser): number {
+    return user.id;
+  }
+
+  addUser() {
+    this.router.navigate(['/users/add']);
+  }
 }
