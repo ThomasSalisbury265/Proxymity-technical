73d668242322a4956e933d941cc0cc1822957099
Tom Salisbury <Tom.Salisbury@oneiota.co.uk>
2024-10-30 12:18:24 +0000

    create reusable user card and user model

--- proxymity-app/src/shared/components/user-card/user-card.component.ts
+++ proxymity-app/src/shared/components/user-card/user-card.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
+import { IUser } from 'src/shared/models/user.model';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
+  providers: [BrowserModule]
 })
 export class UserCardComponent {
+  @Input() user!: IUser;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  onEdit() {
+    this.router.navigate(['/users/edit', this.user.id]);
+  }
+
+  onDelete() {
+    if (confirm(`Are you sure you want to delete ${this.user.name}?`)) {
+      this.userService.deleteUser(this.user.id);
+    }
+  }
 }
